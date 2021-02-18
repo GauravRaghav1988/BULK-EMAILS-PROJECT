57a66968c256ae2adc303db1ddf23223f49c71ed
Gaurav Raghav <78542102+gauravraghav1988@users.noreply.github.com>
2021-02-18 12:59:10 +0530

    Update bulkemails.py

--- bulkemails.py
+++ bulkemails.py
@@ -4,13 +4,13 @@
 import smtplib as s
 ob=s.SMTP("smtp.gmail.com",587)
 ob.starttls()
-password="Fun@1234566"    #Enter your password in between " " to proceed further
+password="Fun@1234566"    #Correct your password to proceed further
 ob.login("saregama20212021",password)
 subject="Greetings...!"
 body="Hello dear..! Gaurav this side welcome you to our new service,\n where we can send multiple emails alltogether.\n This will help you to save your time"
 message="subject:{}\n\n{}".format(subject,body)
 # print(message)
-listofadress=["gauravraghav1988@yahoo.com"]
+listofadress=[""]    # Enter your email ids here to send emails 
 ob.sendmail("gaurav.raghav97@gmail.com",listofadress,message)
 
 print("sent sucussfully.....cheers!!!")
